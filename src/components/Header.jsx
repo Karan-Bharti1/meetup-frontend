eb606221cfc555800939346bb251ed9ea0a657ec
Karan-Bharti1 <karan.codes1@gmail.com>
2024-12-26 13:32:53 +0530

    feat: adding Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom"
+
+const Header=()=>{
+return(
+    <header>
+        <div className="container">
+        <div className=" d-flex align-items-center justify-content-between">
+            <NavLink id="meetupHeading" className={"fst-italic"} to="/">meetup</NavLink>
+            <div> <div className="input-group border">
+  <span className="px-3 bg-white" id="basic-addon1">⌕</span>
+  <input type="text" className="border border-0"  placeholder="Search events and tags..." aria-label="Username" aria-describedby="basic-addon1"/>
+</div></div>
+           
+        </div>
+        <hr/></div>
+     
+    </header>
+)
+}
+export default Header
