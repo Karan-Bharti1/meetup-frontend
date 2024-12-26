8505098dadc4b85ae652a000f09eb0de4dcd85ae
Karan-Bharti1 <karan.codes1@gmail.com>
2024-12-26 12:42:48 +0530

    feat: creating and adding routes for different pages of the app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Description from './pages/Description.js'
+const router=createBrowserRouter([
+  {
+    path:"/",
+  element:<App/>
+  },{
+    path:"/:eventId",
+    element:<Description/>
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+ <RouterProvider router={router}/>
   </StrictMode>,
 )
