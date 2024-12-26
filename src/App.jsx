26d59c2a271e05e778874ede2eccad25e0711361
Karan-Bharti1 <karan.codes1@gmail.com>
2024-12-26 14:02:30 +0530

    feat: fetching events data using useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 import './App.css'
 import Header from './components/Header'
+import useFetch from "./useFetch"
 
 function App() {
-
+const {data,loading,error}=useFetch("https://meetup-backend-coral.vercel.app/events")
+console.log(data)
 
   return (
-    <body style={{backgroundColor:"whitesmoke"}}>
+    <div id="bg-color">
      <Header/>
-    </body>
+    </div>
   )
 }
 
